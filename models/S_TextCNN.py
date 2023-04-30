b574bcfabd500899f72a34783af0e10dc5a01b55
Kaiyu <82535927+loveberryc@users.noreply.github.com>
2023-04-30 19:01:59 +0800

    Update S_TextCNN.py

--- models/S_TextCNN.py
+++ models/S_TextCNN.py
@@ -37,60 +37,6 @@ class Config(object):
         
 
 '''Convolutional Neural Networks for Sentence Classification'''
-
-
-# class Model(nn.Module):
-#     def __init__(self, config):
-#         super(Model, self).__init__()
-#         if config.embedding_pretrained is not None:
-#             self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
-#         else:
-#             self.embedding = nn.Embedding(config.n_vocab, config.embed, padding_idx=config.n_vocab - 1)
-#         self.convs = nn.ModuleList(
-#             [nn.Conv2d(1, config.num_filters, (k, config.embed)) for k in config.filter_sizes])
-#         self.dropout = nn.Dropout(config.dropout)
-#         self.fc = nn.Linear(config.num_filters * len(config.filter_sizes), config.num_classes)
-
-#     def conv_and_pool(self, x, conv):
-#         x = F.relu(conv(x)).squeeze(3)
-#         x = F.max_pool1d(x, x.size(2)).squeeze(2)
-#         return x
-
-#     def forward(self, x):
-#         out = self.embedding(x[0])
-#         out = out.unsqueeze(1)
-#         out = torch.cat([self.conv_and_pool(out, conv) for conv in self.convs], 1)
-#         out = self.dropout(out)
-#         out = self.fc(out)
-#         return out
-
-# class Model(nn.Module):
-#     def __init__(self, config):
-#         super(Model, self).__init__()
-#         if config.embedding_pretrained is not None:
-#             self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
-#         else:
-#             self.embedding = nn.Embedding(config.n_vocab, config.embed, padding_idx=config.n_vocab - 1)
-#         self.convs_1 = nn.ModuleList(
-#             [nn.Conv2d(1, config.num_filters, (k, config.embed)) for k in config.filter_sizes])
-#         self.middle_relu = nn.ModuleList(
-#             [nn.ReLU() for _ in config.filter_sizes])
-#         self.avgpool_1 = nn.ModuleList(
-#             [nn.AvgPool2d((config.pad_size - k + 1, 1)) for k in config.filter_sizes])
-#         self.dropout = nn.Dropout(config.dropout)
-#         self.fc = nn.Linear(config.pad_size, config.num_classes)
-
-#     def forward(self, x):
-#         out = self.embedding(x[0])
-#         out = out.unsqueeze(1)
-#         conv_out = [conv(out) for conv in self.convs_1]
-#         conv_out = [self.middle_relu[i](conv_out[i]) for i in range(len(self.middle_relu))]
-#         pooled_out = [pool(conv_out[i]).squeeze(3) for i, pool in enumerate(self.avgpool_1)]
-#         pooled_out = [pool(pooled_out[i]).squeeze(2) for i, pool in enumerate(self.avgpool_1)]
-#         out = torch.cat(pooled_out, dim=1)
-#         out = self.dropout(out)
-#         out = self.fc(out)
-#         return out
    
 class Model(nn.Module):
     def __init__(self, config):

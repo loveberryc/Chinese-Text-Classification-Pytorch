ac5cb44892308f37536bda04d6480296f71b6902
Kaiyu <82535927+loveberryc@users.noreply.github.com>
2023-05-03 18:12:09 +0800

    Update train_eval.py

--- train_eval.py
+++ train_eval.py
@@ -83,12 +83,11 @@ def train(config, model, train_iter, dev_iter, test_iter):
     writer.close()
     test(config, model, test_iter)
   
-from torch.optim import AdamW
-def train_S(config, model, train_iter, dev_iter, test_iter):
+
+def train(config, model, train_iter, dev_iter, test_iter):
     start_time = time.time()
     model.train()
-#     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-    optimizer = AdamW(model.parameters(), lr=1e-4, weight_decay=0, betas=(0.9, 0.999))
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
 
     # 学习率指数衰减，每次epoch：学习率 = gamma * 学习率
     # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
@@ -100,50 +99,72 @@ def train_S(config, model, train_iter, dev_iter, test_iter):
     for epoch in range(config.num_epochs):
         print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
         # scheduler.step() # 学习率衰减
-        dead_neuron_rate, too_activate_neuron_rate, avg_loss = BPTT(model, train_iter, optimizer=optimizer, criterion=SF.ce_rate_loss(), num_steps=False, time_var=True, time_first=False, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-        print("Dead_neuron_rate in epoch {}: {}.".format(epoch, dead_neuron_rate))
-        print("Too_Activate_neuron_rate in epoch {}: {}.".format(epoch, too_activate_neuron_rate))
-        print("Training epoch {}, avg_loss: {}.".format(epoch, avg_loss))
-
-        dev_acc = predict_accuracy(args, dev_iter, model, 50)
-        print("Dev acc in epoch {} is: {}".format(epoch, dev_acc))
-#         for i, (trains, labels) in enumerate(train_iter):
-#             outputs = model(trains)
-#             model.zero_grad()
-#             loss = F.cross_entropy(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-#             if total_batch % 100 == 0:
-#                 # 每多少轮输出在训练集和验证集上的效果
-#                 true = labels.data.cpu()
-#                 predic = torch.max(outputs.data, 1)[1].cpu()
-#                 train_acc = metrics.accuracy_score(true, predic)
-#                 dev_acc, dev_loss = evaluate(config, model, dev_iter)
-#                 if dev_loss < dev_best_loss:
-#                     dev_best_loss = dev_loss
-#                     torch.save(model.state_dict(), config.save_path)
-#                     improve = '*'
-#                     last_improve = total_batch
-#                 else:
-#                     improve = ''
-#                 time_dif = get_time_dif(start_time)
-#                 msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train Acc: {2:>6.2%},  Val Loss: {3:>5.2},  Val Acc: {4:>6.2%},  Time: {5} {6}'
-#                 print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, improve))
-#                 writer.add_scalar("loss/train", loss.item(), total_batch)
-#                 writer.add_scalar("loss/dev", dev_loss, total_batch)
-#                 writer.add_scalar("acc/train", train_acc, total_batch)
-#                 writer.add_scalar("acc/dev", dev_acc, total_batch)
-#                 model.train()
-#             total_batch += 1
-#             if total_batch - last_improve > config.require_improvement:
-#                 # 验证集loss超过1000batch没下降，结束训练
-#                 print("No optimization for a long time, auto-stopping...")
-#                 flag = True
-#                 break
+        for i, (trains, labels) in enumerate(train_iter):
+            outputs = model(trains)
+            model.zero_grad()
+            loss = F.cross_entropy(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            if total_batch % 100 == 0:
+                # 每多少轮输出在训练集和验证集上的效果
+                true = labels.data.cpu()
+                predic = torch.max(outputs.data, 1)[1].cpu()
+                train_acc = metrics.accuracy_score(true, predic)
+                dev_acc, dev_loss = evaluate(config, model, dev_iter)
+                if dev_loss < dev_best_loss:
+                    dev_best_loss = dev_loss
+                    torch.save(model.state_dict(), config.save_path)
+                    improve = '*'
+                    last_improve = total_batch
+                else:
+                    improve = ''
+                time_dif = get_time_dif(start_time)
+                msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train Acc: {2:>6.2%},  Val Loss: {3:>5.2},  Val Acc: {4:>6.2%},  Time: {5} {6}'
+                print(msg.format(total_batch, loss.item(), train_acc, dev_loss, dev_acc, time_dif, improve))
+                writer.add_scalar("loss/train", loss.item(), total_batch)
+                writer.add_scalar("loss/dev", dev_loss, total_batch)
+                writer.add_scalar("acc/train", train_acc, total_batch)
+                writer.add_scalar("acc/dev", dev_acc, total_batch)
+                model.train()
+            total_batch += 1
+            if total_batch - last_improve > config.require_improvement:
+                # 验证集loss超过1000batch没下降，结束训练
+                print("No optimization for a long time, auto-stopping...")
+                flag = True
+                break
         if flag:
             break
     writer.close()
     test(config, model, test_iter)
+   
+# from torch.optim import AdamW
+# def train_S(config, model, train_iter, dev_iter, test_iter):
+#     start_time = time.time()
+#     model.train()
+# #     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+#     optimizer = AdamW(model.parameters(), lr=1e-4, weight_decay=0, betas=(0.9, 0.999))
+
+#     # 学习率指数衰减，每次epoch：学习率 = gamma * 学习率
+#     # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+#     total_batch = 0  # 记录进行到多少batch
+#     dev_best_loss = float('inf')
+#     last_improve = 0  # 记录上次验证集loss下降的batch数
+#     flag = False  # 记录是否很久没有效果提升
+#     writer = SummaryWriter(log_dir=config.log_path + '/' + time.strftime('%m-%d_%H.%M', time.localtime()))
+#     for epoch in range(config.num_epochs):
+#         print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
+#         # scheduler.step() # 学习率衰减
+#         dead_neuron_rate, too_activate_neuron_rate, avg_loss = BPTT(model, train_iter, optimizer=optimizer, criterion=SF.ce_rate_loss(), num_steps=False, time_var=True, time_first=False, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+#         print("Dead_neuron_rate in epoch {}: {}.".format(epoch, dead_neuron_rate))
+#         print("Too_Activate_neuron_rate in epoch {}: {}.".format(epoch, too_activate_neuron_rate))
+#         print("Training epoch {}, avg_loss: {}.".format(epoch, avg_loss))
+
+#         dev_acc = predict_accuracy(args, dev_iter, model, 50)
+#         print("Dev acc in epoch {} is: {}".format(epoch, dev_acc))
+#         if flag:
+#             break
+#     writer.close()
+#     test(config, model, test_iter)
 
 
 def test(config, model, test_iter):
